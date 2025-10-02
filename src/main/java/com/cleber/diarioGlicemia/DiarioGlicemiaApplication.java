e597b36d643b2b150fe5ed8d19f0c49ceb72a1b6
cleber <silva1cleber@gmail.com>
2025-10-02 11:08:39 -0300

    Firt commit: base do porjeto

---
+++ src/main/java/com/cleber/diarioGlicemia/DiarioGlicemiaApplication.java
@@ -0,0 +1,13 @@
+package com.cleber.diarioGlicemia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioGlicemiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioGlicemiaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cleber/diarioGlicemia/entity/User.java
@@ -0,0 +1,4 @@
+package com.cleber.diarioGlicemia.entity;
+
+public class User {
+}
